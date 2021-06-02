ddaaa2c3e1cb0bd0fb136fc11e9fb8e3172ed42e
WONG CHEE KIN <72852760+IvanWongCheeKin11@users.noreply.github.com>
2021-06-02 08:44:54 +0800

    Add files via upload

---
+++ src/Basketball_Business/BasketballBusiness.java
@@ -0,0 +1,11 @@
+package Basketball_Business;
+
+public class BasketballBusiness {
+    void printBasketballBusiness(String companyName, String address, String phoneNumber, String businessHours, String requirements){
+        System.out.println("Company Name: " + companyName);
+        System.out.println("Company Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Business Hours: " + businessHours);
+        System.out.println("Requirements: " + requirements);
+    }
+}
---
+++ src/Basketball_Business/BasketballBusinessMain.java
@@ -0,0 +1,55 @@
+package Basketball_Business;
+
+public class BasketballBusinessMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Company Details");
+        BasketballBusiness bb = new BasketballBusiness();
+
+        System.out.println("Type Of Sport Basketball");
+        TypeOfSportBasketball a1 = new TypeOfSportBasketball();
+
+        System.out.println();
+        System.out.println("Employees A");
+        Employees eA = new Employees();
+
+        System.out.println();
+        System.out.println("Employees B");
+        Employees eB = new Employees();
+
+        System.out.println();
+        System.out.println("Employees C");
+        Employees eC = new Employees();
+
+        System.out.println();
+        System.out.println("Customer A");
+        Customer cA = new Customer();
+
+        System.out.println();
+        System.out.println("Customer B");
+        Customer cB = new Customer();
+
+        System.out.println();
+        System.out.println("Customer C");
+        Customer cC = new Customer();
+
+        System.out.println();
+        System.out.println("Advertisement Options");
+        AdvertisementAndMarketing b1 = new AdvertisementAndMarketing();
+
+        System.out.println();
+        System.out.println("Basketball Court");
+        BasketballCourt e3 = new BasketballCourt();
+
+        System.out.println();
+        System.out.println("Financial Statement for the first half of the year");
+        Financial f1 = new Financial();
+
+        System.out.println();
+        System.out.println("Financial Statement for the second half of the year");
+        Financial f2 = new Financial();
+
+    }
+
+}
